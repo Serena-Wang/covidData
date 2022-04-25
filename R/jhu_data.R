036be431894fb5f0e38823785c9760108cfc518f
Serena-Wang <wang225y@mtholyoke.edu>
2022-04-25 13:31:51 -0400

    update link base

--- R/jhu_data.R
+++ R/jhu_data.R
@@ -363,7 +363,7 @@ preprocess_jhu_data <- function(issue_date = NULL,
                                 geography = c("US", "global")){
   
   # create url for data update links files
-  link_base <- "https://raw.githubusercontent.com/reichlab/covidData/master/data/"
+  link_base <- "https://raw.githubusercontent.com/Serena-Wang/covidData/master/data/"
   link_file_name <- paste("jhu", tolower(geography[1]), measure, "links", sep = "_")
   # load links
   links <- suppressMessages(readr::read_csv(url(paste0(link_base, link_file_name,".csv"))))
